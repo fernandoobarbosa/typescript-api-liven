3ae825d98f6fa2f61cad7afbadca3e2de066eba0
fernandoobarbosa <71533009+fernandoobarbosa@users.noreply.github.com>
2021-11-20 16:42:04 -0300

    chore: port 3000

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,6 @@ mongoose
     console.log("Error connecting to MongoDB: " + err);
   });
 
-const port = process.env.PORT || 3000;
-
 app.listen(3000, () => {
-  console.log("Server is running on port " + port);
+  console.log("Server is running on port 3000");
 });
