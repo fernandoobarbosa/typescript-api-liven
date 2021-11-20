1c97fc42b307154e714d7a79f6d8e9e4db7a9193
fernandoobarbosa <71533009+fernandoobarbosa@users.noreply.github.com>
2021-11-20 14:51:04 -0300

    chore: swagger with get user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
 import {
   getUserByToken,
   removeByToken,
   login,
   createUser,
 } from "../controllers/user";
-
 import auth from "../middleware/auth";
 
 const app = express();
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/healthcheck", (req: Request, res: Response) => {
   res.status(200).send({ message: "Working!" });
@@ -23,11 +26,11 @@ app.post("/user", (req: Request, res: Response) => {
   createUser(req, res);
 });
 
-app.get("/user/information", auth, (req: Request, res: Response) => {
+app.get("/user", auth, (req: Request, res: Response) => {
   getUserByToken(req, res);
 });
 
-app.delete("/user/remove", auth, (req: Request, res: Response) => {
+app.delete("/user", auth, (req: Request, res: Response) => {
   removeByToken(req, res);
 });
 
