50aadb5ae18f7c61ec7987421652088de6407ac7
fernandoobarbosa <71533009+fernandoobarbosa@users.noreply.github.com>
2021-11-20 17:47:25 -0300

    chore: swagger delete, post and login

--- src/tests/start.test.ts
+++ src/tests/start.test.ts
@@ -4,8 +4,8 @@ import supertest from "supertest";
 
 const request = supertest(app);
 
-it("Health check test", async () => {
-  const response = await request.get("/healthcheck");
+it("Working test", async () => {
+  const response = await request.get("/");
   expect(response.status).toBe(200);
   expect(response.body).toEqual({ message: "Working!" });
 });
