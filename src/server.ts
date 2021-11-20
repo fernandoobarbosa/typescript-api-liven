cc1de59d3f61d59aac64d116a0f478a815bf284c
fernandoobarbosa <71533009+fernandoobarbosa@users.noreply.github.com>
2021-11-20 16:47:45 -0300

    fix: dynamic port

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@ mongoose
     console.log("Error connecting to MongoDB: " + err);
   });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
